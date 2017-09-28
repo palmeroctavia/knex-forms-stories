33ab470a6fa144d40aa428f46210a98cf23f61a5
--global <octaviapalmer@hotmail.com>
2017-09-28 17:27:18 +1300

    working update and add user

--- db.js
+++ db.js
@@ -2,7 +2,8 @@
 module.exports = {
   getUser: getUser,
   getUsers: getUsers,
-  addUser: addUser
+  addUser: addUser,
+  editUser: editUser
 }
 
 function getUsers (connection) {
@@ -10,12 +11,21 @@ function getUsers (connection) {
 }
 
 function getUser (id, connection) {
-  return connection('users').where('id', id)
+  return connection('users').where('id', id).first()
 }
 
 function addUser (data, connection) {
   return connection('users')
     .insert({
+      name: data.name,
+      email: data.email
+    })
+}
+
+function editUser (data, id, connection) {
+  return connection('users')
+    .where('id', id)
+    .update({
       name: data.name,
       email: data.email
     })
--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,37 @@ var router = express.Router()
 
 var db = require('../db')
 
-router.get('/users', function (req, res) {
-  db.getUsers(req.app.get('connection'))
-    .then(function (users) {
-      res.render('index', { users: users })
+// edit existing user
+router.get('/users/:id', (req, res) => {
+  const id = Number(req.params.id)
+  db.getUser(id, req.app.get('connection'))
+    .then(user => {
+      const viewData = {
+        action: '/users/' + id,
+        name: user.name,
+        email: user.email
+      }
+      res.render('formAction', viewData)
+    })
+})
+
+router.post('/users/:id', (req, res) => {
+  const formData = req.body
+  const conn = req.app.get('connection')
+  const id = Number(req.params.id)
+  db.editUser(formData, id, conn)
+    .then(function (formData) {
+      return res.redirect('/users')
     })
     .catch(function (err) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
     })
 })
 
+// add new user
+
 router.get('/users/new', function (req, res) {
-  res.render('form')
+  res.render('formAction', { Action: '/users/new' })
 })
 
 router.post('/users/new', function (req, res) {
@@ -22,6 +41,16 @@ router.post('/users/new', function (req, res) {
   db.addUser(newUserData, req.app.get('connection'))
     .then(function (users) {
       return res.redirect('/users/')
+    })
+    .catch(function (err) {
+      res.status(500).send('DATABASE ERROR: ' + err.message)
+    })
+})
+
+router.get('/users', function (req, res) {
+  db.getUsers(req.app.get('connection'))
+    .then(function (users) {
+      res.render('index', { users: users })
     })
     .catch(function (err) {
       res.status(500).send('DATABASE ERROR: ' + err.message)
